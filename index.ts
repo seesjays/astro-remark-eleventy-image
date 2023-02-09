975be064f28de71ea59bc0e3ca825983bd50bc74
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2023-02-08 18:12:48 -0600

    Updated index.ts to include helper functions

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { remarkEleventyImage } from "./src/astro-remark-images";
+export { createHTML, defaultMarkup } from "./src/markupUtil";
 export type { createHTMLProps, MarkupValues } from "./src/types";
